b6afe28014bf58f380a8d90cecc6c3d5ac3ae0d6
TaylanPostalci <tay.postal@gmail.com>
2022-09-19 15:43:43 -0400

    commit tree cd75bb7e22f538fc0ed0e14ec824f24052e4cf6f

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
+  if (n <= 400){
+return "This one is on me!";
+  }
   // Write your code here!
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(n){
+if (400<n<2000){
+  return "That will be twenty bucks."
+}
   // Write your code here!
 }
 
